e3a1d3b32e92905422e7fb10af3d1bc5d0b23c50
jittathep kohchapong <fishlnwmak3@gmail.com>
2020-10-03 17:52:02 +0700

    webserviceVIAget

--- src/main/java/th/ac/ku/atm/service/BankAccountService.java
+++ src/main/java/th/ac/ku/atm/service/BankAccountService.java
@@ -1,15 +1,36 @@
 package th.ac.ku.atm.service;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 import th.ac.ku.atm.model.BankAccount;
 
 import javax.annotation.PostConstruct;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Arrays;
 
 @Service
 public class BankAccountService {
     private List<BankAccount> bankAccounts;
+    private RestTemplate restTemplate;
+
+    public BankAccountService(RestTemplate restTemplate) {
+        this.restTemplate = restTemplate;
+    }
+
+    public List<BankAccount> getCustomerBankAccount(int customerId) {
+        String url = "http://localhost:8091/api/bankaccount/customer/" +
+                customerId;
+       ResponseEntity<BankAccount[]> response =
+                restTemplate.getForEntity(url, BankAccount[].class);
+
+        BankAccount[] accounts = response.getBody();
+
+        return Arrays.asList(accounts);
+
+    }
+
     @PostConstruct
     public void BankAccountService(){
         bankAccounts = new ArrayList<>();
